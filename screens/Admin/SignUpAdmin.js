6945c7620a6b4b439d4359165328fc01f2aa5380
Hammad <hammadghaffar14@gmail.com>
2020-12-27 17:45:17 +0500

    changed

--- screens/Admin/SignUpAdmin.js
+++ screens/Admin/SignUpAdmin.js
@@ -36,66 +36,11 @@ const SignUpAdmin = props =>
         }
     );
     const [isVisible,setisVisible] = useState(false);
-    function generateRandomString(length){
-        var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*()';
-        var random_string = '';
-        if(length > 0){
-          for(var i=0; i < length; i++){
-              random_string += chars.charAt(Math.floor(Math.random() * chars.length));
-          }
-        }
-        return random_string;
-        }
+   
     const confirmHandler = () =>
     {
         if(true/*check with database if username,email or phone number is same or not */){
             /*if it is not same,execute these commands*/
-            /* Jab user signup pe click karey or sari details check hojaen to database me enter krne se pehle aik verification ka
-            page show ho aur udr 5 length ki string input karega user "vcode" jo neche create horahi hai. usko aik variable me store karao
-            or vcode se check karado same hogi to database me store hojaega warna verification code dubara mangey.*/
-            const vcode = generateRandomString(5);
-            app.post('/send',(req,res) => {
-                const output = `</p>Welcome to Fast Foods. We recieved a registration request on our app.</p>
-                <u1>
-                <li>Name: ${user.Username}</li>
-                </u1>
-                <h3>Message</h3>
-                <p> Kindly enter this verification code in the app </p>
-                <p> ${vcode}<p>
-                `;
-            });
-            // create reusable transporter object using the default SMTP transport
-            let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true, // true for 465, false for other ports
-            auth: {
-            user: 'k181056@nu.edu.pk', // generated ethereal user
-            pass: 'bingoregister./', // generated ethereal password
-            },
-            /* localhost par chalana ho to ye command uncommend krdena 
-            tls:{
-            rejectUnauthorized:false
-            } */
-            });
-            // setup email data with unicode symbols
-            let mailOptions = {
-            from: '"Fast Foods" <k181056@nu.edu.pk>', // sender address
-            to: user.email, // receiver
-            subject: "Verification Email for Registration in Fast Foods", // Subject line
-            text: "Hello user req.body.username We recieved a request for Registration of an account in our App Fast Foods. Kindly enter this Verification code in the App: ", // plain text body
-            html: output, // html body
-            }
-            // send mail with defined transport object
-            transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              return console.log(error);
-            }
-            console.log("Message sent: %s", info.messageId);
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-            });
-            
-            
             console.log(user);
             setisVisible(true);
         }
