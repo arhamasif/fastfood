aa1c012fca33b3c1bbbea9ce9b5d924025614408
arhamasif <65038849+arhamasif@users.noreply.github.com>
2020-12-27 17:46:29 +0500

    server removed

---
+++ components/UnderlineExample.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from 'react-native-confirmation-code-field';
+
+import styles from './stylesVer';
+
+const CELL_COUNT = 5;
+
+const UnderlineExample = () => {
+  const [value, setValue] = useState('');
+  const ref = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue,
+  });
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <Text style={styles.title}>Underline example</Text>
+      <CodeField
+        ref={ref}
+        {...props}
+        value={value}
+        onChangeText={setValue}
+        cellCount={CELL_COUNT}
+        rootStyle={styles.codeFieldRoot}
+        keyboardType="number-pad"
+        textContentType="oneTimeCode"
+        renderCell={({index, symbol, isFocused}) => (
+          <View
+            // Make sure that you pass onLayout={getCellOnLayoutHandler(index)} prop to root component of "Cell"
+            onLayout={getCellOnLayoutHandler(index)}
+            key={index}
+            style={[styles.cellRoot, isFocused && styles.focusCell]}>
+            <Text style={styles.cellText}>
+              {symbol || (isFocused ? <Cursor /> : null)}
+            </Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default UnderlineExample;
